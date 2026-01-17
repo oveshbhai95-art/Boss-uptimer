ccd88e52e88c215facc4d3028166694be0772a0b
oveshbhai95-art <oveshbhai95@gmail.com>
2026-01-17 16:13:50 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+
+class Config:
+    API_ID = int(os.environ.get("API_ID", "23903140"))
+    API_HASH = os.environ.get("API_HASH", "579f1bcf3eac1660d81ef34b09906012")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    MONGO_URL = os.environ.get("MONGO_URL", "")
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1003166629808"))
